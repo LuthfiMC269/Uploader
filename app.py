f245d2dc4e17b3f803e04b084330ef00a0547cd0
Luhfimc <luthfim269@gmail.com>
2025-07-24 00:53:45 +0700

    initial

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, url_for, send_from_directory, render_template, jsonify
 from werkzeug.utils import secure_filename
+import threading
 import os
 import uuid
 import time
-from datetime import datetime, timedelta
 
-UPLOAD_FOLDER = os.path.join(os.getcwd(), "static", "uploads")
+UPLOAD_FOLDER = os.path.join("static", "uploads")
 EXPIRATION_TIME = 3 * 60 * 60  # 3 jam
 ALLOWED_EXTENSIONS = {
     'jpg', 'jpeg', 'png', 'gif', 'bmp',       # Gambar
@@ -13,10 +13,6 @@ ALLOWED_EXTENSIONS = {
     'pdf', 'doc', 'docx', 'xls', 'xlsx',      # Dokumen
     'txt', 'zip', 'rar'                       # File umum
 }
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -24,18 +20,26 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-def cleanup_old_files():
-    now = time.time()
-    for filename in os.listdir(UPLOAD_FOLDER):
-        filepath = os.path.join(UPLOAD_FOLDER, filename)
-        if os.path.isfile(filepath):
-            if now - os.path.getmtime(filepath) > EXPIRATION_TIME:
-                os.remove(filepath)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def cleanup_old_files():
+    while True:
+        now = time.time()
+        for filename in os.listdir(UPLOAD_FOLDER):
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+            if os.path.isfile(filepath):
+                if now - os.path.getmtime(filepath) > EXPIRATION_TIME:
+                    try:
+                        os.remove(filepath)
+                        print(f"[INFO] Deleted: {filename}")
+                    except Exception as e:
+                        print(f"[ERROR] {e}")
+        time.sleep(60)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    cleanup_old_files()
     if request.method == 'POST':
         if 'image' not in request.files:
             return 'No file part'
@@ -54,7 +58,6 @@ def index():
             return render_template('index.html', image_url=image_url)
         else:
             return "File tidak diizinkan.", 400
-
     return render_template('index.html')
 
 @app.route('/uploads/<filename>')
@@ -62,5 +65,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    threading.Thread(target=cleanup_old_files, daemon=True).start()
+    app.run(debug=False, host='0.0.0.0')
 
